df564a70dc1892f60887454a2c79264270f90059
José Miguel Oroño <joseomaker@gmail.com>
2024-01-20 02:54:12 -0500

    Create background.ts & open static site on install

---
+++ background.ts
@@ -0,0 +1,14 @@
+export {}
+ 
+console.log(
+  "Background Worker Active. You can now use the extension"
+)
+
+chrome.runtime.onInstalled.addListener(({reason}) => {
+  if (reason === 'install') {
+    chrome.tabs.create({
+      url: "static/onboarding.html"
+    });
+  }
+});
+
