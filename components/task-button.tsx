1b1a8190720a9ff2ad10b90debcb3f9cee66770e
José Miguel Oroño <joseomaker@gmail.com>
2024-11-04 04:06:28 -0500

    Create task-button.tsx

---
+++ components/task-button.tsx
@@ -0,0 +1,56 @@
+import Button from "@mui/material/Button"
+import { styled } from "@mui/material/styles"
+import { useMutation } from "@tanstack/react-query"
+import toast from "react-hot-toast"
+
+interface ITaskButtonProps {
+  text: string
+  successText: Nullable<string>
+  errorText: Nullable<string>
+  loadingText?: string
+  fnQuery: Function
+  disabled?: boolean
+  variant?: "text" | "outlined" | "contained"
+}
+
+export default function TaskButton({
+  text,
+  successText = null,
+  errorText = null,
+  loadingText = "Loading...",
+  fnQuery,
+  disabled = false,
+  variant = "contained"
+}: ITaskButtonProps) {
+  const mutation = useMutation({
+    mutationFn: () => fnQuery(),
+    // Disable caching by setting cacheTime to 0
+    gcTime: 0,
+    // Don't retry on failure
+    retry: 0
+  })
+
+  if (mutation.isSuccess) {
+    console.log("Mutation success")
+    toast.success(successText || "Task completed successfully.")
+  }
+
+  if (mutation.isError) {
+    console.error(errorText)
+    console.error(mutation.error)
+    toast.error(errorText || "An error occurred.")
+  }
+
+  const handleClick = () => {
+    mutation.mutate()
+  }
+
+  return (
+    <Button
+      variant={variant}
+      onClick={handleClick}
+      disabled={disabled || mutation.isPending}>
+      {mutation.isPending ? loadingText : text}
+    </Button>
+  )
+}
