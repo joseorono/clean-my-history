efe344775adc7beb02a1910a4543077232612ba4
José Miguel Oroño <joseomaker@gmail.com>
2024-11-03 22:31:10 -0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -12,91 +12,87 @@
 
 // https://www.cisdem.com/resource/list-of-websites-to-block-at-work.html
 
+export const nsfwKeywords: string[] = [
+  "porn",
+  "adult",
+  "xxx",
+  "nsfw",
+  "sex",
+  "hentai",
+  "rule34",
+  "fetish",
+  "nude",
+  "xvid",
+  "xham",
+  "kink",
+  "dirty",
+  "naughty",
+  "milf"
+]
 
-export const nsfwKeywords: string[]  = [
-    "porn",
-    "adult",
-    "xxx",
-    "nsfw",
-    "sex",
-    "hentai",
-    "rule34",
-    "fetish",
-    "nude",
-    "xvid",
-    "xham",
-    "kink",
-    "dirty",
-    "naughty",
-    "milf",
-];
-
-// I've excluded Social Media sites that might be useful for work, like 
+// I've excluded Social Media sites that might be useful for work, like
 const socialMediaKeywords: string[] = [
-    "facebook",
-    "twitter",
-    "instagram",
-    "tiktok",
-    "snapchat",
-    "reddit",
-    "tumblr",
-    "myspace",
-    "whatsapp",
-    "telegram",
-    "wechat",
-    "line",
-    "kik",
-    "omegle",
-    "periscope",
-    "meowchat",
-    "bumble",
-    "grindr",
-    "okcupid",
-    "tinder",
-    "match",
-    "hinge",
-];
+  "facebook",
+  "twitter",
+  "instagram",
+  "tiktok",
+  "snapchat",
+  "reddit",
+  "tumblr",
+  "myspace",
+  "whatsapp",
+  "telegram",
+  "wechat",
+  "line",
+  "kik",
+  "omegle",
+  "periscope",
+  "meowchat",
+  "bumble",
+  "grindr",
+  "okcupid",
+  "tinder",
+  "match",
+  "hinge"
+]
 
 const gamingKeywords: string[] = [
-    "game",
-    "gaming",
-    "gamer",
-    "ign",
-    "roblox",
-    "play",
-    "gaming",
-    "playstation",
-    "psx",
-    "ps2",
-    "ps3",
-    "ps4",
-    "ps5",
-    "nintendo",
-    "steampowered",
-    "fortnite",
-    "steamdeck",
-    "xbox",
-    "steam",
-    "online",
-    "esports",
-    "console",
-    "gamble",
-    "mmo",
-    "multiplayer",
-    "arcade",
-    "rpg",
-    "fps",
-    "joystick",
-    "twitch",
-    
-];
+  "game",
+  "gaming",
+  "gamer",
+  "ign",
+  "roblox",
+  "play",
+  "gaming",
+  "playstation",
+  "psx",
 
+  "ps2",
+  "ps3",
+  "ps4",
+  "ps5",
+  "nintendo",
+  "steampowered",
+  "fortnite",
+  "steamdeck",
+  "xbox",
+  "steam",
+  "online",
+  "esports",
+  "console",
+  "gamble",
+  "mmo",
+  "multiplayer",
+  "arcade",
+  "rpg",
+  "fps",
+  "joystick",
+  "twitch"
+]
 
 // The Spread operator is a perfomrance killer. We can probably optimize this later.
 export const allBadKeywords = [
-    ...nsfwKeywords,
-    ...gamingKeywords,
-    ...socialMediaKeywords
-];
-
-
+  ...nsfwKeywords,
+  ...gamingKeywords,
+  ...socialMediaKeywords
+]
