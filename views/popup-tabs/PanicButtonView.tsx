e399f896ac87d84b3dde9a166a28811d9dfc4b2c
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2024-11-04 22:55:19 -0400

    Create Panic Buttom

--- views/popup-tabs/PanicButtonView.tsx
+++ views/popup-tabs/PanicButtonView.tsx
@@ -1,14 +1,24 @@
-export default function PanicButtonView() {
+export default function AllTabsView() {
   return (
     <>
-      <div>
-        <h1>Panic Button</h1>
-      </div>
-
-      <div className="text-jet mt-6 rounded-xl border bg-slate-300 p-4 shadow">
-        <h3>Beware!</h3>
-        <p>This is a work in progress. It's not ready yet.</p>
-      </div>
+      <button className="flex h-12 w-full cursor-pointer items-center justify-center gap-2 rounded-md bg-gradient-to-r from-[#fb7185] via-[#e11d48] to-[#be123c] px-4 font-semibold text-white shadow-2xl duration-300 hover:scale-105 hover:from-[#be123c] hover:to-[#fb7185] hover:shadow-xl hover:shadow-red-500">
+        Panic Buttom
+        <svg viewBox="0 0 15 15" className="w-5 fill-white">
+          <svg
+            className="h-6 w-6"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+              stroke-linejoin="round"
+              stroke-linecap="round"></path>
+          </svg>
+          Button
+        </svg>
+      </button>
     </>
   );
 }
