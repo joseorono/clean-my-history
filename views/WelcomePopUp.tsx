62ec8c2f2b1dc60553b60641f1d3ce529e0e9645
José Miguel Oroño <joseomaker@gmail.com>
2025-12-02 02:20:41 -0500

    Update WelcomePopUp.tsx

--- views/WelcomePopUp.tsx
+++ views/WelcomePopUp.tsx
@@ -1,7 +1,9 @@
 export default function WelcomePopUp() {
   return (
     <>
-      <div id="popup-top" className="flex items-center justify-between p-4">
+      <div
+        id="popup-top"
+        className="flex items-center justify-between p-4 bg-[rgb(25,118,210)] text-white">
         <h2 className="text-xl font-bold">
           Welcome to{" "}
           <a href="https://www.plasmo.com" target="_blank">
