92aae1fa75b98f0458524314042285840f2affda
Andreina Di Brigida <Akiarakirax3@gmail.com>
2025-12-27 21:06:43 -0400

    Add colors to tailwind conf and css variables
    Co-Authored-By: José Miguel Oroño <joseomaker@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,28 @@ module.exports = {
     "./static/**/*.html"
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--color-primary-main)',
+          light: 'var(--color-primary-light)',
+          dark: 'var(--color-primary-dark)'
+        },
+        secondary: {
+          DEFAULT: 'var(--color-secondary-main)',
+          light: 'var(--color-secondary-light)',
+          dark: 'var(--color-secondary-dark)'
+        },
+        background: {
+          DEFAULT: 'var(--color-background-default)',
+          paper: 'var(--color-background-paper)'
+        },
+        text: {
+          primary: 'var(--color-text-primary)',
+          secondary: 'var(--color-text-secondary)'
+        }
+      }
+    }
   },
   plugins: []
 };
